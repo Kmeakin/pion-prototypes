43406a0f64b6fcf00df45342a97252650cd0ed35
Karl Meakin <karlwfmeakin@gmail.com>
2023-07-12 23:49:39 +0100

    report syntax errors

--- crates/pion/src/check.rs
+++ crates/pion/src/check.rs
@@ -1,6 +1,7 @@
 use anyhow::bail;
 use ariadne::{Label, Report, ReportKind, Source};
 use pion_lexer::token::TokenError;
+use pion_utils::interner::Interner;
 use pion_utils::source::{SourceFile, SourceMap};
 
 #[derive(clap::Args)]
@@ -61,7 +62,104 @@ pub fn run(args: Args) -> anyhow::Result<()> {
         }
     }
 
+    let interner = Interner::new();
+    let bump = bumpalo::Bump::new();
+
+    for (_file_id, file) in source_map.iter() {
+        let (_module, errors) =
+            pion_surface::syntax::parse_module(&file.contents, &bump, &interner);
+
+        for error in errors {
+            let report = match error {
+                pion_surface::reporting::SyntaxError::Lexer((span, error)) => match error {
+                    TokenError::UnknownCharacter => Report::build(ReportKind::Error, &file.path, 0)
+                        .with_message("syntax error")
+                        .with_label(
+                            Label::new((&file.path, std::ops::Range::from(span)))
+                                .with_message("unknown character"),
+                        ),
+                    TokenError::BlockComment { .. } => {
+                        Report::build(ReportKind::Error, &file.path, 0)
+                            .with_message("syntax error")
+                            .with_label(
+                                Label::new((&file.path, std::ops::Range::from(span)))
+                                    .with_message("unclosed block comment"),
+                            )
+                    }
+                },
+                pion_surface::reporting::SyntaxError::InvalidToken(span) => {
+                    Report::build(ReportKind::Error, &file.path, 0)
+                        .with_message("syntax error")
+                        .with_label(
+                            Label::new((&file.path, std::ops::Range::from(span)))
+                                .with_message("invalid token"),
+                        )
+                }
+                pion_surface::reporting::SyntaxError::UnrecognizedEof {
+                    span,
+                    expected_tokens,
+                } => {
+                    let mut report = Report::build(ReportKind::Error, &file.path, 0)
+                        .with_message("syntax error")
+                        .with_label(
+                            Label::new((&file.path, std::ops::Range::from(span)))
+                                .with_message("unexpected end of file"),
+                        );
+                    if let Some(note) = format_expected(&expected_tokens) {
+                        report = report.with_note(note);
+                    }
+                    report
+                }
+                pion_surface::reporting::SyntaxError::UnrecognizedToken {
+                    span,
+                    found_token,
+                    expected_tokens,
+                } => {
+                    let mut report = Report::build(ReportKind::Error, &file.path, 0)
+                        .with_message("syntax error")
+                        .with_label(
+                            Label::new((&file.path, std::ops::Range::from(span)))
+                                .with_message("unexpected token {found_token}"),
+                        );
+                    if let Some(note) = format_expected(&expected_tokens) {
+                        report = report.with_note(note);
+                    }
+                    report
+                }
+                pion_surface::reporting::SyntaxError::ExtraToken { span, found_token } => {
+                    Report::build(ReportKind::Error, &file.path, 0)
+                        .with_message("syntax error")
+                        .with_label(
+                            Label::new((&file.path, std::ops::Range::from(span)))
+                                .with_message("unexpected token {found_token}"),
+                        )
+                }
+            };
+
+            report
+                .finish()
+                .eprint((&file.path, Source::from(&file.contents)))?;
+
+            error_count += 1;
+        }
+    }
+
     stop_if_errors(error_count)?;
 
     Ok(())
+}
+
+fn format_expected(expected: &[String]) -> Option<String> {
+    match expected {
+        [] => None,
+        [first] => Some(format!("help: expected {first}")),
+        [first, rest @ .., last] => {
+            let mut out = format!("help: expected one of {first}");
+            for expected in rest {
+                out.push_str(&format!(", {expected}"));
+            }
+            out.push_str(&format!(" or {last}"));
+            Some(out)
+        }
+    }
 }
