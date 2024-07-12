797c48c450f54f06bce3d0f1ea3b2a623d094e91
Karl Meakin <karlwfmeakin@gmail.com>
2024-07-12 17:07:48 +0100

    semantics: quote

--- crates/pion-core/src/semantics.rs
+++ crates/pion-core/src/semantics.rs
@@ -189,13 +189,119 @@ pub fn apply_closure<'core>(
     eval(body, bump, opts, &mut local_values, meta_values)
 }
 
+pub fn quote<'core>(
+    value: &Value<'core>,
+    bump: &'core bumpalo::Bump,
+    local_len: EnvLen,
+    meta_values: &MetaValues<'core>,
+) -> Result<Expr<'core>, Error<'core>> {
+    match value {
+        Value::Error => Ok(Expr::Error),
+        Value::Lit(lit) => Ok(Expr::Lit(*lit)),
+        Value::Neutral(head, spine) => {
+            let head = quote_head(*head, bump, local_len, meta_values)?;
+            spine.iter().try_fold(head, |head, elim| match elim {
+                Elim::FunApp(arg) => {
+                    let arg_expr = quote(&arg.expr, bump, local_len, meta_values)?;
+                    let (fun, arg_expr) = bump.alloc((head, arg_expr));
+                    let arg = FunArg::new(arg.plicity, &*arg_expr);
+                    Ok(Expr::FunApp { fun, arg })
+                }
+            })
+        }
+        Value::FunType { param, body } => {
+            let (param, body) = quote_fun(*param, body, bump, local_len, meta_values)?;
+            Ok(Expr::FunType { param, body })
+        }
+        Value::FunLit { param, body } => {
+            let (param, body) = quote_fun(*param, body, bump, local_len, meta_values)?;
+            Ok(Expr::FunLit { param, body })
+        }
+    }
+}
+
+fn quote_head<'core>(
+    head: Head,
+    bump: &'core bumpalo::Bump,
+    local_len: EnvLen,
+    meta_values: &MetaValues<'core>,
+) -> Result<Expr<'core>, Error<'core>> {
+    match head {
+        Head::LocalVar(var) => match local_len.absolute_to_relative(var) {
+            Some(var) => Ok(Expr::LocalVar(var)),
+            None => todo!("Unbound local var: {var:?}"),
+        },
+        Head::MetaVar(var) => match meta_values.get_absolute(var) {
+            Some(Some(value)) => quote(value, bump, local_len, meta_values),
+            Some(None) => Ok(Expr::MetaVar(var)),
+            None => Err(Error::UnboundMetaVar {
+                var,
+                len: meta_values.len(),
+            }),
+        },
+    }
+}
+
+fn quote_fun<'core>(
+    param: FunParam<&'core Value<'core>>,
+    closure: &Closure<'core>,
+    bump: &'core bumpalo::Bump,
+    local_len: EnvLen,
+    meta_values: &MetaValues<'core>,
+) -> Result<(FunParam<&'core Expr<'core>>, &'core Expr<'core>), Error<'core>> {
+    let r#type = quote(param.r#type, bump, local_len, meta_values)?;
+
+    let arg = Value::local_var(local_len.to_absolute());
+    let body = apply_closure(
+        closure.clone(),
+        arg,
+        bump,
+        EvalOpts::for_quote(),
+        meta_values,
+    )?;
+    let body = quote(&body, bump, local_len.succ(), meta_values)?;
+
+    let (r#type, body) = bump.alloc((r#type, body));
+
+    Ok((FunParam::new(param.plicity, r#type), body))
+}
+
+pub fn update_metas<'core>(
+    value: &Value<'core>,
+    bump: &'core bumpalo::Bump,
+    meta_values: &MetaValues<'core>,
+) -> Result<Value<'core>, Error<'core>> {
+    let mut value = value.clone();
+    while let Value::Neutral(Head::MetaVar(var), spine) = value {
+        match meta_values.get_absolute(var) {
+            None => {
+                return Err(Error::UnboundMetaVar {
+                    var,
+                    len: meta_values.len(),
+                })
+            }
+            Some(None) => return Ok(Value::Neutral(Head::MetaVar(var), spine)),
+            Some(Some(head)) => {
+                value = spine
+                    .into_iter()
+                    .try_fold(head.clone(), |head, elim| match elim {
+                        Elim::FunApp(arg) => {
+                            fun_app(head, arg, bump, EvalOpts::for_quote(), meta_values)
+                        }
+                    })?;
+            }
+        }
+    }
+    Ok(value)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::env::UniqueEnv;
 
     #[track_caller]
-    fn check_eval(expr: Expr, expected: Result<Value, Error>) {
+    fn assert_eval(expr: Expr, expected: Result<Value, Error>) {
         let bump = bumpalo::Bump::default();
         let mut local_values = LocalValues::default();
         let meta_values = UniqueEnv::default();
@@ -210,18 +316,28 @@ mod tests {
         assert_eq!(value, expected);
     }
 
+    #[track_caller]
+    fn assert_quote(value: Value, expected: Result<Expr, Error>) {
+        let bump = bumpalo::Bump::default();
+        let local_len = EnvLen::default();
+        let meta_values = UniqueEnv::default();
+
+        let expr = quote(&value, &bump, local_len, &meta_values);
+        assert_eq!(expr, expected);
+    }
+
     #[test]
-    fn eval_error() { check_eval(Expr::Error, Ok(Value::Error)); }
+    fn eval_error() { assert_eval(Expr::Error, Ok(Value::Error)); }
 
     #[test]
     fn eval_lit() {
-        check_eval(Expr::Lit(Lit::Int(10)), Ok(Value::Lit(Lit::Int(10))));
-        check_eval(Expr::Lit(Lit::Char('a')), Ok(Value::Lit(Lit::Char('a'))));
+        assert_eval(Expr::Lit(Lit::Int(10)), Ok(Value::Lit(Lit::Int(10))));
+        assert_eval(Expr::Lit(Lit::Char('a')), Ok(Value::Lit(Lit::Char('a'))));
     }
 
     #[test]
     fn eval_let() {
-        check_eval(
+        assert_eval(
             Expr::Let {
                 r#type: &Expr::Error,
                 rhs: &Expr::Lit(Lit::Int(10)),
@@ -233,7 +349,7 @@ mod tests {
 
     #[test]
     fn eval_unbound_local_var() {
-        check_eval(
+        assert_eval(
             Expr::LocalVar(RelativeVar::new(0)),
             Err(Error::UnboundLocalVar {
                 var: RelativeVar::new(0),
@@ -241,7 +357,7 @@ mod tests {
             }),
         );
 
-        check_eval(
+        assert_eval(
             Expr::Let {
                 r#type: &Expr::Error,
                 rhs: &Expr::Lit(Lit::Int(10)),
@@ -256,7 +372,7 @@ mod tests {
 
     #[test]
     fn eval_unbound_meta_var() {
-        check_eval(
+        assert_eval(
             Expr::MetaVar(AbsoluteVar::new(0)),
             Err(Error::UnboundMetaVar {
                 var: AbsoluteVar::new(0),
@@ -273,7 +389,7 @@ mod tests {
         };
         let arg = FunArg::explicit(&Expr::Lit(Lit::Int(10)));
         let expr = Expr::FunApp { fun: &fun, arg };
-        check_eval(expr, Ok(Value::Lit(Lit::Int(10))));
+        assert_eval(expr, Ok(Value::Lit(Lit::Int(10))));
     }
 
     #[test]
@@ -284,7 +400,7 @@ mod tests {
         };
         let arg = FunArg::explicit(&Expr::Lit(Lit::Int(10)));
         let expr = Expr::FunApp { fun: &fun, arg };
-        check_eval(
+        assert_eval(
             expr,
             Err(Error::FunAppPlicityMismatch {
                 param_plicity: Plicity::Implicit,
@@ -298,7 +414,7 @@ mod tests {
         };
         let arg = FunArg::implicit(&Expr::Lit(Lit::Int(10)));
         let expr = Expr::FunApp { fun: &fun, arg };
-        check_eval(
+        assert_eval(
             expr,
             Err(Error::FunAppPlicityMismatch {
                 param_plicity: Plicity::Explicit,
@@ -312,7 +428,7 @@ mod tests {
         let fun = Expr::Lit(Lit::Char('a'));
         let arg = FunArg::explicit(&Expr::Lit(Lit::Int(10)));
         let expr = Expr::FunApp { fun: &fun, arg };
-        check_eval(
+        assert_eval(
             expr,
             Err(Error::FunAppHeadNotFun {
                 head: Value::Lit(Lit::Char('a')),
@@ -325,6 +441,44 @@ mod tests {
         let fun = Expr::Error;
         let arg = FunArg::explicit(&Expr::Lit(Lit::Int(10)));
         let expr = Expr::FunApp { fun: &fun, arg };
-        check_eval(expr, Ok(Value::Error));
+        assert_eval(expr, Ok(Value::Error));
+    }
+
+    #[test]
+    fn quote_error() { assert_quote(Value::Error, Ok(Expr::Error)); }
+
+    #[test]
+    fn quote_lit() { assert_quote(Value::Lit(Lit::Int(10)), Ok(Expr::Lit(Lit::Int(10)))); }
+
+    #[test]
+    fn quote_fun_lit() {
+        let body = Expr::LocalVar(RelativeVar::new(0));
+        let fun = Value::FunLit {
+            param: FunParam::explicit(&Value::Error),
+            body: Closure::empty(&body),
+        };
+        assert_quote(
+            fun,
+            Ok(Expr::FunLit {
+                param: FunParam::explicit(&Expr::Error),
+                body: &Expr::LocalVar(RelativeVar::new(0)),
+            }),
+        );
+    }
+
+    #[test]
+    fn quote_fun_type() {
+        let body = Expr::LocalVar(RelativeVar::new(0));
+        let fun = Value::FunType {
+            param: FunParam::explicit(&Value::Error),
+            body: Closure::empty(&body),
+        };
+        assert_quote(
+            fun,
+            Ok(Expr::FunType {
+                param: FunParam::explicit(&Expr::Error),
+                body: &Expr::LocalVar(RelativeVar::new(0)),
+            }),
+        );
     }
 }
