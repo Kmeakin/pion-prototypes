940775bb82e4ce1d496ff8b7edbbc70eb3522f0d
Karl Meakin <karlwfmeakin@gmail.com>
2024-09-24 03:32:04 +0100

    rewrite small step evaluator as a visitor

--- lambda/src/eval.rs
+++ lambda/src/eval.rs
@@ -13,3 +13,19 @@ fn get_local<'core>(env: &Env<'core>, var: usize, name: &str) -> Value<'core> {
         None => panic!("Unbound variable `{name}` (index {var} in environment of length {len})"),
     }
 }
+
+impl Binop {
+    pub fn apply<'core>(self, lhs: u32, rhs: u32) -> Value<'core> {
+        match self {
+            Self::Add => Value::Int(u32::wrapping_add(lhs, rhs)),
+            Self::Sub => Value::Int(u32::wrapping_sub(lhs, rhs)),
+            Self::Mul => Value::Int(u32::wrapping_mul(lhs, rhs)),
+            Self::Eq => Value::Bool(u32::eq(&lhs, &rhs)),
+            Self::Ne => Value::Bool(u32::ne(&lhs, &rhs)),
+            Self::Le => Value::Bool(u32::le(&lhs, &rhs)),
+            Self::Ge => Value::Bool(u32::ge(&lhs, &rhs)),
+            Self::Lt => Value::Bool(u32::lt(&lhs, &rhs)),
+            Self::Gt => Value::Bool(u32::gt(&lhs, &rhs)),
+        }
+    }
+}
--- lambda/src/eval/small_step.rs
+++ lambda/src/eval/small_step.rs
@@ -1,4 +1,3 @@
-
 use super::*;
 
 pub enum Control<'core> {
@@ -20,6 +19,8 @@ pub enum Frame<'core> {
     If1(&'core Expr<'core>, &'core Expr<'core>),
 }
 
+pub type Kont<'core> = Vec<Frame<'core>>;
+
 pub fn push_kont<'core>(mut kont: Kont<'core>, frame: Frame<'core>) -> Kont<'core> {
     kont.push(frame);
     kont
@@ -30,73 +31,174 @@ pub fn push_env<'core>(mut env: Env<'core>, value: Value<'core>) -> Env<'core> {
     env
 }
 
-type Kont<'core> = Vec<Frame<'core>>;
-
-#[rustfmt::skip]
-    pub fn step<'core>(
+pub trait EvalVisitor<'core>: Sized {
+    fn step(
+        &mut self,
         control: Control<'core>,
-        mut env: Env<'core>,
-        mut kont: Kont<'core>,
+        env: Env<'core>,
+        kont: Kont<'core>,
     ) -> (Control<'core>, Env<'core>, Kont<'core>) {
-        match control {
-            Control::Expr(Expr::Int(n)) => (Control::Value(Value::Int(n)), env, kont),
-            Control::Expr(Expr::Bool(b)) => (Control::Value(Value::Bool(b)), env, kont),
-            Control::Expr(Expr::Var(var, name)) => (Control::Value(get_local(&env, var, name)), env, kont),
-            Control::Expr(Expr::Fun(name, body)) => (Control::Value(Value::Fun(name, env.clone(), body)), env, kont),
-            Control::Expr(Expr::App(fun, arg)) => (Control::Expr(*fun), env, push_kont(kont, Frame::App1(arg))),
-            Control::Expr(Expr::Binop(op, lhs, rhs)) => (Control::Expr(*lhs), env, push_kont(kont, Frame::Binop1(op, rhs))),
-            Control::Expr(Expr::Let(name, init, body)) => (Control::Expr(*init), env, push_kont(kont, Frame::Let1(name, body))),
-            Control::Expr(Expr::If(cond, then, r#else)) => (Control::Expr(*cond), env, push_kont(kont, Frame::If1(then, r#else))),
-            Control::Value(value) => match kont.pop() {
-                None => (Control::Value(value), env, kont),
-                Some(Frame::App1(arg)) => (Control::Expr(*arg), env, push_kont(kont, Frame::App2(value))),
-                Some(Frame::App2(fun)) => match fun {
-                    Value::Fun(_name, new_env, body) => (Control::Expr(*body), push_env(new_env, value), push_kont(kont, Frame::App3(env))),
-                    _ => panic!("Expected function, got {fun}"),
-                },
-                Some(Frame::App3(old_env)) => (Control::Value(value), old_env, kont),
-                Some(Frame::Binop1(op, rhs)) => (Control::Expr(*rhs),env, push_kont(kont, Frame::Binop2(op, value))),
-                Some(Frame::Binop2(op, lhs)) => {
-                    let rhs = value;
-                    let (Value::Int(lhs), Value::Int(rhs)) = (&lhs, &rhs) else {
-                        panic!("Expected integers, got {lhs} and {rhs}");
-                    };
-                    let (lhs, rhs) = (*lhs, *rhs);
-                    let value = match op {
-                        Binop::Add => Value::Int(u32::wrapping_add(lhs, rhs)),
-                        Binop::Sub => Value::Int(u32::wrapping_sub(lhs, rhs)),
-                        Binop::Mul => Value::Int(u32::wrapping_mul(lhs, rhs)),
-                        Binop::Eq => Value::Bool(u32::eq(&lhs, &rhs)),
-                        Binop::Ne => Value::Bool(u32::ne(&lhs, &rhs)),
-                        Binop::Le => Value::Bool(u32::le(&lhs, &rhs)),
-                        Binop::Ge => Value::Bool(u32::ge(&lhs, &rhs)),
-                        Binop::Lt => Value::Bool(u32::lt(&lhs, &rhs)),
-                        Binop::Gt => Value::Bool(u32::gt(&lhs, &rhs)),
-                    };
-                    (Control::Value(value), env, kont)
-                }
-                Some(Frame::Let1(_name, body)) => (Control::Expr(*body), push_env(env, value), push_kont(kont, Frame::Let2)),
-                Some(Frame::Let2) => { env.pop(); (Control::Value(value), env, kont) }
-                Some(Frame::If1(then, r#else)) => match value {
-                    Value::Bool(true) => (Control::Expr(*then), env, kont),
-                    Value::Bool(false) => (Control::Expr(*r#else), env, kont),
-                    _ => panic!("Expected boolean, got {value}"),
-                },
-            },
-        }
+        step(self, control, env, kont)
     }
 
-pub fn eval<'core>(expr: Expr<'core>, mut env: Env<'core>) -> Value<'core> {
-    let mut control = Control::Expr(expr);
-    let mut kont = Vec::new();
+    fn eval(&mut self, expr: Expr<'core>, env: Env<'core>, kont: Kont<'core>) -> Value<'core> {
+        eval(self, expr, env, kont)
+    }
 
+    fn var_reduced(
+        &mut self,
+        _var: (usize, &'core str),
+        value: Value<'core>,
+        env: Env<'core>,
+        kont: Kont<'core>,
+    ) -> (Control<'core>, Env<'core>, Kont<'core>) {
+        (Control::Value(value), env, kont)
+    }
+
+    fn app_reduced(
+        &mut self,
+        (_name, new_env, body): (&'core str, Env<'core>, &'core Expr<'core>),
+        arg: Value<'core>,
+        env: Env<'core>,
+        kont: Kont<'core>,
+    ) -> (Control<'core>, Env<'core>, Kont<'core>) {
+        (
+            Control::Expr(*body),
+            push_env(new_env, arg),
+            push_kont(kont, Frame::App3(env)),
+        )
+    }
+
+    fn binop_reduced(
+        &mut self,
+        (op, lhs, rhs): (Binop, u32, u32),
+        env: Env<'core>,
+        kont: Kont<'core>,
+    ) -> (Control<'core>, Env<'core>, Kont<'core>) {
+        (Control::Value(op.apply(lhs, rhs)), env, kont)
+    }
+
+    fn let_reduced(
+        &mut self,
+        (_name, init, body): (&'core str, Value<'core>, &'core Expr<'core>),
+        env: Env<'core>,
+        kont: Kont<'core>,
+    ) -> (Control<'core>, Env<'core>, Kont<'core>) {
+        (
+            Control::Expr(*body),
+            push_env(env, init),
+            push_kont(kont, Frame::Let2),
+        )
+    }
+
+    fn if_reduced(
+        &mut self,
+        cond: bool,
+        then: &'core Expr<'core>,
+        r#else: &'core Expr<'core>,
+        env: Env<'core>,
+        kont: Kont<'core>,
+    ) -> (Control<'core>, Env<'core>, Kont<'core>) {
+        match cond {
+            true => (Control::Expr(*then), env, kont),
+            false => (Control::Expr(*r#else), env, kont),
+        }
+    }
+}
+
+pub fn eval<'core, V: EvalVisitor<'core>>(
+    visitor: &mut V,
+    expr: Expr<'core>,
+    mut env: Env<'core>,
+    mut kont: Kont<'core>,
+) -> Value<'core> {
+    let mut control = Control::Expr(expr);
     loop {
-        match step(control, env, kont) {
+        match visitor.step(control, env, kont) {
             (Control::Value(value), _, kont) if kont.is_empty() => return value,
             cek => (control, env, kont) = cek,
         }
     }
 }
+
+pub fn step<'core, V: EvalVisitor<'core>>(
+    visitor: &mut V,
+    control: Control<'core>,
+    mut env: Env<'core>,
+    mut kont: Kont<'core>,
+) -> (Control<'core>, Env<'core>, Kont<'core>) {
+    match control {
+        Control::Expr(Expr::Int(n)) => (Control::Value(Value::Int(n)), env, kont),
+        Control::Expr(Expr::Bool(b)) => (Control::Value(Value::Bool(b)), env, kont),
+        Control::Expr(Expr::Var(var, name)) => {
+            let value = get_local(&env, var, name);
+            visitor.var_reduced((var, name), value, env, kont)
+        }
+        Control::Expr(Expr::Fun(name, body)) => (
+            Control::Value(Value::Fun(name, env.clone(), body)),
+            env,
+            kont,
+        ),
+        Control::Expr(Expr::App(fun, arg)) => {
+            (Control::Expr(*fun), env, push_kont(kont, Frame::App1(arg)))
+        }
+        Control::Expr(Expr::Binop(op, lhs, rhs)) => (
+            Control::Expr(*lhs),
+            env,
+            push_kont(kont, Frame::Binop1(op, rhs)),
+        ),
+        Control::Expr(Expr::Let(name, init, body)) => (
+            Control::Expr(*init),
+            env,
+            push_kont(kont, Frame::Let1(name, body)),
+        ),
+        Control::Expr(Expr::If(cond, then, r#else)) => (
+            Control::Expr(*cond),
+            env,
+            push_kont(kont, Frame::If1(then, r#else)),
+        ),
+        Control::Value(value) => match kont.pop() {
+            None => (Control::Value(value), env, kont),
+            Some(Frame::App1(arg)) => (
+                Control::Expr(*arg),
+                env,
+                push_kont(kont, Frame::App2(value)),
+            ),
+            Some(Frame::App2(fun)) => match fun {
+                Value::Fun(name, new_env, body) => {
+                    visitor.app_reduced((name, new_env, body), value, env, kont)
+                }
+                _ => panic!("Expected function, got {fun}"),
+            },
+            Some(Frame::App3(old_env)) => (Control::Value(value), old_env, kont),
+            Some(Frame::Binop1(op, rhs)) => (
+                Control::Expr(*rhs),
+                env,
+                push_kont(kont, Frame::Binop2(op, value)),
+            ),
+            Some(Frame::Binop2(op, lhs)) => {
+                let rhs = value;
+                let (Value::Int(lhs), Value::Int(rhs)) = (&lhs, &rhs) else {
+                    panic!("Expected integers, got {lhs} and {rhs}");
+                };
+                let (lhs, rhs) = (*lhs, *rhs);
+                visitor.binop_reduced((op, lhs, rhs), env, kont)
+            }
+            Some(Frame::Let1(name, body)) => visitor.let_reduced((name, value, body), env, kont),
+            Some(Frame::Let2) => {
+                env.pop();
+                (Control::Value(value), env, kont)
+            }
+            Some(Frame::If1(then, r#else)) => match value {
+                Value::Bool(cond) => visitor.if_reduced(cond, then, r#else, env, kont),
+                _ => panic!("Expected boolean, got {value}"),
+            },
+        },
+    }
+}
+
+pub struct DefaultVisitor;
+impl<'core> EvalVisitor<'core> for DefaultVisitor {}
 
 #[cfg(test)]
 mod tests {
@@ -106,8 +208,10 @@ mod tests {
 
     #[track_caller]
     fn assert_eval(expr: &Expr, expect: Expect) {
+        let mut visitor = DefaultVisitor;
         let env = Env::new();
-        let value = eval(*expr, env);
+        let kont = Kont::new();
+        let value = eval(&mut visitor, *expr, env, kont);
         expect.assert_eq(&format!("{value}"));
     }
 
